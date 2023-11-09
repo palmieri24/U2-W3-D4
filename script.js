198b857aef199d9567c05eec4832b2c71f9c4815
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2023-11-09 16:49:47 +0100

    wip cards

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const URL = "https://api.pexels.com/v1/search?query=beach";
 const apiKey = "Mj1aTwwXCbW0nrTCwih7oe3mxhSATqYUKTAVbEde6iKOTcDN2XhUKPnY";
 const loadImagesBtn = document.getElementById("loadImages");
 const loadSecondaryImagesBtn = document.getElementById("loadSecondaryImages");
+const card = document.getElementsByClassName("card");
 
 window.onload = () => {
   fetch(URL, {
@@ -9,11 +10,19 @@ window.onload = () => {
   })
     .then((resp) => resp.json())
     .then((resourceObj) => {
-      const { url, photographer, alt } = resourceObj;
-
-      document.getElementById("url").value = url;
-      document.getElementById("photographer").value = photographer;
-      document.getElementById("alt").value = alt;
+      console.log(resourceObj);
+      resourceObj.forEach((elem) => {
+        const imgCard = document.createElement("img");
+        imgCard.src = elem.url;
+        imgCard.className = "card-img-top object-fit-cover img-fluid";
+        card.appendChild(imgCard);
+      });
     })
     .catch((err) => console.log(err));
 };
+
+//const { url, photographer, alt } = resourceObj;
+
+//   document.getElementById("url").value = url;
+// document.getElementById("photographer").value = photographer;
+//document.getElementById("alt").value = alt;
