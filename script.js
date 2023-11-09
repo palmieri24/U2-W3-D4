81573a8cc402957206c3f97915c76b9f470cb7b8
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2023-11-09 15:58:19 +0100

    wip second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const URL = "https://api.pexels.com/v1/search?query=beach";
 const apiKey = "Mj1aTwwXCbW0nrTCwih7oe3mxhSATqYUKTAVbEde6iKOTcDN2XhUKPnY";
+const loadImagesBtn = document.getElementById("loadImages");
+const loadSecondaryImagesBtn = document.getElementById("loadSecondaryImages");
 
 window.onload = () => {
   fetch(URL, {
@@ -8,5 +10,10 @@ window.onload = () => {
     .then((resp) => resp.json())
     .then((resourceObj) => {
       const { url, photographer, alt } = resourceObj;
-    });
+
+      document.getElementById("url").value = url;
+      document.getElementById("photographer").value = photographer;
+      document.getElementById("alt").value = alt;
+    })
+    .catch((err) => console.log(err));
 };
